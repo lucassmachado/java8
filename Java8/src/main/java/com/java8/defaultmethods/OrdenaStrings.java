74177ded51fbf1387304559c13c6b0a56e8c8d84
Lucas Machado <lucas.machado290@gmail.com>
2020-08-25 20:13:24 -0300

    add default methods examples

---
+++ Java8/src/main/java/com/java8/defaultmethods/OrdenaStrings.java
@@ -0,0 +1,52 @@
+package com.java8.defaultmethods;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class OrdenaStrings {
+
+	public static void main(String[] args) {
+		List<String> palavras = Arrays.asList("alura online", "editora casa do código", "caelum");
+		
+		Comparator<String> comparador = new ComparadorPorTamanho();
+		Collections.sort(palavras, comparador);
+		System.out.println(palavras);
+		
+		palavras.sort(comparador);
+		
+		for (String p : palavras) {
+			System.out.println(p);
+		}
+		
+		palavras.forEach(new ImprimeNaLinha());
+	}
+	
+}
+
+class ImprimeNaLinha implements Consumer<String> {
+
+	@Override
+	public void accept(String t) {
+		System.out.println(t);
+	}
+	
+}
+
+class ComparadorPorTamanho implements Comparator<String> {
+
+	@Override
+	public int compare(String s1, String s2) {
+		if (s1.length() < s2.length()) {
+			return -1;
+		}
+		if (s1.length() > s2.length()) {
+			return 1;
+		}
+		return 0;
+	}
+	
+}
